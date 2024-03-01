768630be7b731f673b8ee541acbe161bbc9a3426
Samiksha <mailtosamiksha.s@gmail.com>
2024-03-01 10:19:03 +0000

    initial commit:created display rules function

--- run.py
+++ run.py
@@ -1,3 +1,24 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+import random 
+"""
+The command to import random module for functions
+"""
+
+def display_rules():
+    rules = """
+
+    Hi There! 
+    Game rules are :
+    
+    - Rock smashes scissors 
+    - Paper covers rock
+    - Scissors cut paper.
+
+    """
+    print(rules)
+
+
+display_rules()
