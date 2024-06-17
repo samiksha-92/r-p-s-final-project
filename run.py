a06c6f642cacdaccd202f2a34e9ea0173fd8b395
Samiksha <mailtosamiksha.s@gmail.com>
2024-06-17 08:34:20 +0000

    fixed more than 20 indentation errors and changed the variable name list to options to avoid shadowing the built in list type

--- run.py
+++ run.py
@@ -2,13 +2,16 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
-import random 
+import random
 
 """
 The command to import random module for functions
 """
+
+
 def display_rules():
-    rules = "Hi There, Game rules are: Rock smashes scissors, Paper covers rock & Scissors cut paper"
+    rules = ("Hi There, Game rules are: Rock smashes scissors, Paper covers "
+             "rock & Scissors cut paper")
     print(rules)
 
 
@@ -16,48 +19,52 @@ def get_computer_move():
     """
     This function randomly selects a value from this list
     """
-    computer_move = random.choice(['rock', 'paper', 'scissors'])   
+    computer_move = random.choice(['rock', 'paper', 'scissors'])
     return computer_move
 
 
 def get_your_move():
     """
-    This function will ask user for an input and validate if the input is valid.
+    This function will ask user for an input and validate
     """
     while True:
         your_move = input("Choose between rock, paper or scissors:\n")
-        list = ["rock", "paper", "scissors"]
+        options = ["rock", "paper", "scissors"]
 
         if not your_move:
             print("Invalid answer, please give an answer")
-        elif your_move not in list:
+        elif your_move not in options:
             print("Invalid answer, please read the game rules again")
         else:
-            return your_move    
+            return your_move
 
 
-def display_results(computer_move,your_move):  
+def display_results(computer_move, your_move):
     if your_move == computer_move:
         print("Its a tie")
-    elif (your_move == "rock" and computer_move == "scissors") or (your_move == "paper" and computer_move == "rock") or (your_move == "scissors" and computer_move == "paper"):
-        print(f"Congratulations, you win! {your_move.capitalize()} beats {computer_move.capitalize()}")
+    elif ((your_move == "rock" and computer_move == "scissors") or
+          (your_move == "paper" and computer_move == "rock") or
+          (your_move == "scissors" and computer_move == "paper")):
+        print(f"Congratulations, you win! {your_move.capitalize()} beats "
+              f"{computer_move.capitalize()}")
     else:
-        print(f"You lost! {computer_move.capitalize()} beats {your_move.capitalize()}")
+        print(f"You lost! {computer_move.capitalize()} beats "
+              f"{your_move.capitalize()}")
 
 
 def game_flow():
     """
-    This function executes the game flow and includes helper functions declared above
+    This function executes the game flow and includes helper functions
     """
     display_rules()
 
-    Opponent_move = get_computer_move()
+    opponent_move = get_computer_move()
 
     player_move = get_your_move()
 
-    display_results(Opponent_move, player_move)
+    display_results(opponent_move, player_move)
 
-    return Opponent_move, player_move
+    return opponent_move, player_move
 
 
 def main():
@@ -65,37 +72,38 @@ def main():
     This function keeps track of scores
     """
 
-    #Initialisation of scores
-
+    # Initialisation of scores
     computer_wins = 0
     player_wins = 0
-    tie_score = 0    
- # Starting game loop
+    tie_score = 0
+
+    # Starting game loop
     while True:
-      Opponent_move,player_move = game_flow()
+        opponent_move, player_move = game_flow()
 
-   #Updating scores
-      if Opponent_move == player_move:
-        tie_score += 1
-        print(f"Computer selected {Opponent_move.capitalize()}\n")
-      elif (player_move == "rock" and Opponent_move == "scissors") or (player_move == "paper" and Opponent_move == "rock") or (player_move == "scissors" and Opponent_move == "paper"):
-         player_wins += 1
-         print(f"Computer selected {Opponent_move.capitalize()}\n")
-      else:
-        computer_wins += 1
-        print()
-        print(f"Computer selected {Opponent_move.capitalize()}\n")
+        # Updating scores
+        if opponent_move == player_move:
+            tie_score += 1
+            print(f"Computer selected {opponent_move.capitalize()}\n")
+        elif ((player_move == "rock" and opponent_move == "scissors") or
+              (player_move == "paper" and opponent_move == "rock") or
+              (player_move == "scissors" and opponent_move == "paper")):
+            player_wins += 1
+            print(f"Computer selected {opponent_move.capitalize()}\n")
+        else:
+            computer_wins += 1
+            print(f"Computer selected {opponent_move.capitalize()}\n")
 
-   #Displaying results
-      print(f"\nScores - Computer: {computer_wins}, Player: {player_wins}, Tie: {tie_score}\n")
+        # Displaying results
+        print(f"\nScores - Computer: {computer_wins}, Player: {player_wins}, "
+              f"Tie: {tie_score}\n")
 
-    #Asking if player wants to play again
-      wanna_play_again = input("Enter any key to play another round or press q to quit\n")
-      if wanna_play_again == 'q':
-        print("Thanks for playing, Goodbye!")
-        break
+        # Asking if player wants to play again
+        wanna_play_again = input("Enter any key to play another round or press "
+                                 "q to quit\n")
+        if wanna_play_again == 'q':
+            print("Thanks for playing, Goodbye!")
+            break
 
-      
+
 main()
-
-
